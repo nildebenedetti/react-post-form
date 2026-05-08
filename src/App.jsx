3fd7e495bb1adea3c3fa0d20ee341d1e56c21d3a
“Nilde <nilde.benedetti@gmail.com>
2026-05-08 15:40:26 +0200

    add: primo input del form con relativa label in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,23 @@
 
 function App() {
   return <section className="add-post-form">
-  <div className="container">
-    <div className="card m-3 p-2">
-      <h4 className="card-title">Aggiungi un post</h4>
-      <form>
-
-      </form>
+    <div className="container">
+      <div className="card m-3 p-2">
+        <h4 className="card-title">Aggiungi un post</h4>
+        <form>
+          {/**
+            author (string) - L’autore del post
+            title (string) - Il titolo del post
+            body (string) - Il testo del post
+            public (boolean) - Se il post deve essere
+                pubblico (true) o una bozza (false)
+         */}
+          <label htmlFor="author">Autore</label>
+          <input type="text" id="author" name="author" onChange="" value="">
+          </input>
+        </form>
+      </div>
     </div>
-  </div>
   </section>
 }
 export default App;
