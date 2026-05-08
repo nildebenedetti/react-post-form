a1b977c598986fac97e1fed0244e91c8c06a4b48
“Nilde <nilde.benedetti@gmail.com>
2026-05-08 16:06:01 +0200

    add: tutti gli input del form in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   // variabile di stato per registrare i dati del form
   // ci inserisco un ogetto che rappresenta il form per mappare le chiavi
-  const [ formData, setFormData ] = useState({
+  const [formData, setFormData] = useState({
     author: "nome autore",
   });
 
@@ -13,18 +13,30 @@ function App() {
   return <section className="add-post-form">
     <div className="container">
       <div className="card m-3 p-2">
-        <h4 className="card-title">Aggiungi un post</h4>
-        <form>
+        <h4 className="card-title">Aggiungi un Post</h4>
+        <form className="d-flex flex-column">
           {/**
             author (string) - L’autore del post
             title (string) - Il titolo del post
             body (string) - Il testo del post
             public (boolean) - Se il post deve essere
                 pubblico (true) o una bozza (false)
+
+          n.b.: id e'rilevante per matchare la label, 
+          name e'rilevante per recuperare la value
          */}
-          <label htmlFor="author">Autore</label>
-          <input type="text" id="author" name="author" onChange="" value="">
+          <label htmlFor="author">Author</label>
+          <input type="text" id="author" name="postAuthor" onChange="" value="">
           </input>
+          <label htmlFor="title">Title </label>
+          <input type="text" id="title" name="PostTitle" onChange="" value=""></input>
+          <br></br>
+          <label htmlFor="body">Your Post </label>
+          <textarea type="textarea" id="body" rows={10} name="title" onChange="" value=""></textarea>
+          <div>
+            <label htmlFor="public">Publish </label>
+            <input type="checkbox" name="publishState" id="public" onChange="" checked></input>
+          </div>
         </form>
       </div>
     </div>
