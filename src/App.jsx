6ecadca349e29246aa65dafd097c200e4f1d149a
“Nilde <nilde.benedetti@gmail.com>
2026-05-08 15:32:42 +0200

    add: scheletro della sezione add post form (pure semantica, TVB Samuel) in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 
 
 function App() {
-  return
+  return <section className="add-post-form">
+  <div className="container">
+    <div className="card m-3 p-2">
+      <h4 className="card-title">Aggiungi un post</h4>
+      <form>
+
+      </form>
+    </div>
+  </div>
+  </section>
 }
 export default App;
