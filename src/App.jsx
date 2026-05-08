b40896e0ac5e4f0608d4cb196153c504c841a0e5
“Nilde <nilde.benedetti@gmail.com>
2026-05-08 16:55:12 +0200

    add: aggiungo variabile keytioupdate e terna logica per smistamento tiupo input in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,23 +16,26 @@ function App() {
   const changeHandler = (event) => {
     // devo identificare chi ha scatenato il cambiamento
     const target = event.target;
+    //controllo se e'di tipo check e in caso se e' settato
+    const checked = target.checked;
     // poi mi salvo il suo VALORE e NOME
     // NOME mi serve per cercare nell'obj form data la chiave
-    // VALORE mi serve per sovrascrivere il valore vecchio
-    const { name, value } = target;
+    // VALORE/ checked mi serve per sovrascrivere il valore vecchio
+    const { name, value, type, } = target;
+    const keyToUpdate = (type === 'checkbox' ? checked : value)
+    // inserisco il value to update di modo da smistare campi checkbox e campi name
     // ottenuti nome e valore, creo COPIA
     // di formData con spreadOperator
     // e sostituisco quello che devo cambiare
     const newFormData = {
       ...formData,
-      [name]: value
+      [keyToUpdate]: value
     };
     // POI setto il nuovo valore
-    console.log(newFormData);
-    
+    setFormData(newFormData);
     // RISULTATO: ogni volta che cambio una virgola nel form, lo rifletto su UI
-    
-    };
+
+  };
 
   return <section className="add-post-form">
     <div className="container">
@@ -48,21 +51,21 @@ function App() {
 
           n.b.: id e'rilevante per matchare la label, 
           name e'rilevante per recuperare la value
+          la value, recupera a sua volta i dati dal form
          */}
           <div>
             <label htmlFor="author">Author</label>
-            <input type="text" id="author" name="postAuthor" onChange="" value="">
+            <input type="text" id="author" name="postAuthor" onChange={changeHandler} value={formData.postAuthor}>
             </input>
-
             <label htmlFor="title">Title </label>
-            <input type="text" id="title" name="PostTitle" onChange="" value=""></input>
+            <input type="text" id="title" name="PostTitle" onChange={changeHandler} value={formData.postTitle}></input>
             <br></br>
           </div>
           <label htmlFor="body">Your Post </label>
-          <textarea type="textarea" id="body" rows={10} name="postBody" onChange="" value=""></textarea>
+          <textarea type="textarea" id="body" rows={10} name="postBody" onChange={changeHandler} value={formData.postBody}></textarea>
           <div>
             <label htmlFor="public">Publish </label>
-            <input type="checkbox" name="publishState" id="public" onChange="" checked></input>
+            <input type="checkbox" name="publishState" id="public" onChange={changeHandler} checked={formData.publishState.checked}></input>
           </div>
         </form>
       </div>
